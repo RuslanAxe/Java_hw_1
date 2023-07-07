94c6c11ff0113f7b35a6365c03303e6260966351
Ruslan Shakirov <vikingsonsturm@gmail.com>
2023-07-07 12:56:16 +0300

    попытался сделать задачу 2

--- task2.java
+++ task2.java
@@ -1,8 +1,34 @@
 // Задание
 // 2) К калькулятору из предыдущего дз добавить логирование.
+import java.util.Arrays;
 import java.util.Scanner;
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
 public class task2 {
-    public static void main(String[] args) {
+   public static void Logger(String fileName) throws IOException {
+        // Создаем логгер
+        Logger logger = Logger.getLogger(task1.class.getName());
+//        logger.setLevel(Level.INFO);
+        // вывод
+//        ConsoleHandler ch = new ConsoleHandler();
+//        logger.addHandler(ch);
+        FileHandler fh = new FileHandler(fileName);
+        logger.addHandler(fh);
+        // формат вывода
+//        SimpleFormatter sFormatter = new SimpleFormatter();
+        XMLFormatter xml = new XMLFormatter();
+//        ch.setFormatter(sFormatter);
+//        ch.setFormatter(xml);
+        fh.setFormatter(xml);
+
+        //Сообщение
+        logger.log(Level.INFO, "Test logging");
+        logger.info("Test logging2");
+   }
+    public static void calculation(String[] args) {
       double num1;
       double num2;
       double ans;
@@ -27,5 +53,14 @@ public class task2 {
       }
       System.out.print("\nРезультат операции:\n");
       System.out.printf(num1 + " " + op + " " + num2 + " = " + ans);
-    } 
-}
+      String fileName = "LogCalculation.xml";
+      Logger(fileName);
+      System.out.println(Double.toString(ans, fileName));
+   }
+   // public static void calculation(String[] args) throws IOException {
+   //      //Придумываем имя для логгера и записываем логи в xml
+   //      String fileName = "LogCalculation.xml";
+   //      Logger(fileName);
+   //    }
+   }
+
